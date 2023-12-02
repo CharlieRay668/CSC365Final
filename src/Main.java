e6b7195307b918d09574d86d1a00c87c7cc4ce25
Nick Li <nmli@calpoly.edu>
2023-12-02 01:32:05 -0800

    "integrated javafx into main, started a stage, now idea how the rest of it works"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+public class Main extends Application {
+
+    public void start(Stage stage)throws Exception{
+        Group root =new Group();
+        Scene scene = new Scene(root,Color.LIGHTSLATEGREY);
+        stage.setTitle("Test search engine");
+        stage.setScene(scene);
+        stage.show();
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        launch(args);
     }
 }
