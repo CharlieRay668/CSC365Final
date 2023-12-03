1a5141b1c971736a1c513ecb55f80379e34991b3
Charlie Ray <charlie.ray320@gmail.com>
2023-12-02 23:42:36 -0800

    Refactored APIPuller

--- src/APIPuller.java
+++ src/APIPuller.java
@@ -8,18 +8,44 @@ import java.io.IOException;
 
 
 public class APIPuller {
-    public String getAccessToken() throws IOException, InterruptedException {
-        String spotify_secret = "14a40f1ebfbc43d4a68b737567c950d7";
-        String client_id = "0f94df5707024ee49ceb1144705cc75c";
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://accounts.spotify.com/api/token"))
-                .POST(BodyPublishers.ofString("grant_type=client_credentials&client_id="+client_id+"&client_secret="+spotify_secret))
-                .setHeader("Content-Type", "application/x-www-form-urlencoded")
-                .build();
+    private HttpClient client;
+    private String spotify_secret;
+    private String client_id;
+    public APIPuller() {
+        this.spotify_secret = "14a40f1ebfbc43d4a68b737567c950d7";
+        this.client_id = "0f94df5707024ee49ceb1144705cc75c";
+        this.client = HttpClient.newHttpClient();
+    }
 
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    public String makePostRequest(String url, String body, String header) throws IOException, InterruptedException {
+        String[] headerArr = header.split(":");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .POST(BodyPublishers.ofString(body))
+                .setHeader(headerArr[0], headerArr[1])
+                .build();
+        HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
         String data = response.body();
+        return data;
+    }
+
+    public String makeGetReqeust(String url, String header) throws IOException, InterruptedException {
+        String[] headerArr = header.split(":");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .setHeader(headerArr[0], headerArr[1])
+                .build();
+        HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
+        String data = response.body();
+        return data;
+    }
+
+    public String getAccessToken() throws IOException, InterruptedException {
+        String url = "https://accounts.spotify.com/api/token";
+        String body = "grant_type=client_credentials&client_id="+client_id+"&client_secret="+spotify_secret;
+        String header = "Content-Type:application/x-www-form-urlencoded";
+        String data = makePostRequest(url, body, header);
 //        parse data for access token data is JSON
         String[] dataArr = data.split(",");
         String[] tokenArr = dataArr[0].split(":");
@@ -29,29 +55,17 @@ public class APIPuller {
 
     public String searchArtist(String artist) throws IOException, InterruptedException {
         String token = getAccessToken();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.spotify.com/v1/search?q="+artist+"&type=artist"))
-                .GET()
-                .setHeader("Authorization", "Bearer "+token)
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String data = response.body();
+        String url = "https://api.spotify.com/v1/search?q="+artist+"&type=artist";
+        String header = "Authorization:Bearer "+token;
+        String data = makeGetReqeust(url, header);
         return data;
     }
 
     public String getArtist(String artistID) throws IOException, InterruptedException {
         String token = getAccessToken();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.spotify.com/v1/artists/"+artistID))
-                .GET()
-                .setHeader("Authorization", "Bearer "+token)
-                .build();
-
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        String data = response.body();
+        String url = "https://api.spotify.com/v1/artists/"+artistID;
+        String header = "Authorization:Bearer "+token;
+        String data = makeGetReqeust(url, header);
         return data;
     }
     public static void main(String[] args) throws IOException, InterruptedException {
--- src/SQLConnector.java
+++ src/SQLConnector.java
@@ -1,12 +1,60 @@
 import java.sql.*;
 
 class SQLConnector {
-    static   Connection connect;
-    public static void main(String[] args) {
+    static private Connection connect;
+
+    public SQLConnector() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection(
+            this.connect = DriverManager.getConnection(
                     "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306?user=chray&password=talltree");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertGenre(String genre) {
+        try {
+            Statement stmt = this.connect.createStatement();
+            stmt.executeUpdate("INSERT INTO chray.genres (genre) VALUES ('"+genre+"');");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertArtist(String artist, String artistID, int popularity, String uri, int followers) {
+        try {
+            Statement stmt = this.connect.createStatement();
+            stmt.executeUpdate("INSERT INTO chray.Artist (aid, name, popularity, uri, followers) VALUES ('"+artistID+"','"+artist+"',"+popularity+",'"+uri+"',"+followers+");");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void instertTrack(String tid, int duration, boolean explicit, String name, String uri) {
+        try {
+            Statement stmt = this.connect.createStatement();
+            stmt.executeUpdate("INSERT INTO chray.Track (tid, duration, explicit, name, uri) VALUES ('"+tid+"',"+duration+","+explicit+",'"+name+"','"+uri+"');");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertAlbum(String albumID, String type, int totalTracks, String name, String releaseDate, String uri) {
+        try {
+            Statement stmt = connect.createStatement();
+            stmt.executeUpdate("INSERT INTO chray.Album (albumID, type, totalTracks, name, releaseDate, uri) VALUES ('"+albumID+"','"+type+"',"+totalTracks+",'"+name+"','"+releaseDate+"','"+uri+"');");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void insertPlaylist(String pid, String name) {
+        try {
+            Statement stmt = connect.createStatement();
+            stmt.executeUpdate("INSERT INTO chray.Playlist (pid, name) VALUES ('"+pid+"','"+name+"');");
         } catch (Exception e) {
             e.printStackTrace();
         }
