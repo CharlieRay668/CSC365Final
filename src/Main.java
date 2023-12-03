d732ea2c8a6fcf57e1a10d02548163cad4e246b2
Charlie Ray <charlie.ray320@gmail.com>
2023-12-02 23:12:11 -0800

    connected

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-public class Main extends Application {
+// import javafx.application.Application;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Group;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.stage.Stage;
+// import javafx.scene.paint.Color;
+public class Main { //extends Application {
 
-    public void start(Stage stage)throws Exception{
-        Group root =new Group();
-        Scene scene = new Scene(root,Color.LIGHTSLATEGREY);
-        stage.setTitle("Test search engine");
-        stage.setScene(scene);
-        stage.show();
-    }
+    // public void start(Stage stage)throws Exception{
+    //     Group root =new Group();
+    //     Scene scene = new Scene(root,Color.LIGHTSLATEGREY);
+    //     stage.setTitle("Test search engine");
+    //     stage.setScene(scene);
+    //     stage.show();
+    // }
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        launch(args);
+        // launch(args);
     }
 }
--- src/SQLConnector.java
+++ src/SQLConnector.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class SQLConnector {
+import java.sql.*;
+
+class SQLConnector {
+    static   Connection connect;
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(
+                    "jdbc:mysql://ambari-node5.csc.calpoly.edu:3306?user=chray&password=talltree");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
